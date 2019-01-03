85adf7ecbd41d42044681d4da220ad7cc45f40bc
Dhani <dhaniekonugroho@gmail.com>
2019-01-03 10:48:23 +0000

    Edit existing file

--- ubuntu_18_10.sh
+++ ubuntu_18_10.sh
@@ -10,15 +10,12 @@ sudo apt install gnome-tweaks chrome-gnome-shell
 echo "Creating icons and themes forlder under Home directory"
 mkdir ~/.icons
 mkdir ~/.themes
-#Download McOS Space-Grey theme and Mac OS Icon Theme
-cd /tmp
-#cloning the git repository
-git clone https://github.com/dhaniekonugroho/mac-theme-script.git
+#Extract files
 tar -xvf DARK.for.Dash.to.DOCK.tar.xz
 tar -xvf McOS-Space-Grey-3.30.tar.xz
 tar -xvf MacOSX-icon-theme.tar.xz
-cp -R /tmp/DARK.for.Dash.to.DOCK/3.30/Majove_NIGHT ~/.themes
-mv /tmp/MacOSX ~/.icons
-mv /tmp/McOS-Space-Grey-3.30 ~/.themes
+cp -R DARK.for.Dash.to.DOCK/3.30/Majove_NIGHT ~/.themes
+mv MacOSX ~/.icons
+mv McOS-Space-Grey-3.30 ~/.themes
 gsettings set org.gnome.desktop.interface gtk-theme "McOS-Space-Grey-3.30"
 gsettings set org.gnome.desktop.interface icon-theme 'MacOSX'
